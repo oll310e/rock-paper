7ab5240eebc1e5882c0ec078609ce3bd09527084
oll310e <o.anderzen@gmail.com>
2023-01-02 15:56:39 +0100

    Add getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hellow callow world')
+const getComputerChoice = function(){
+    let randomAnswer = Math.floor(Math.random() * 3) + 1;
+    console.log(randomAnswer)
+    switch (randomAnswer) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
+}
+
+console.log(getComputerChoice())
