e76889f5ebcd7f1d68b08c19f73ce8fc990026eb
oll310e <o.anderzen@gmail.com>
2023-01-02 16:11:48 +0100

    Add gameStart function returning win,loss or draw. Add prompt for playerChoice

--- index.js
+++ index.js
@@ -11,4 +11,49 @@ const getComputerChoice = function(){
     }
 }
 
-console.log(getComputerChoice())
+
+let computerChoice = getComputerChoice()
+console.log(computerChoice)
+
+let playerChoice = prompt('what is you choice?').toLowerCase()
+
+console.log(playerChoice)
+
+const gameStart = function(playerSelection, computerSelection){
+    let result;
+    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors'){
+        return 'please make a valid input';
+    }
+    switch (playerSelection){
+        case 'paper':
+            if (computerSelection == 'rock') {
+                result = 'win'
+            } else if (computerSelection == 'scissors'){
+                result = 'loss'
+            } else {
+                result = 'draw'
+            }
+            break;
+        case 'rock':
+            if (computerSelection == 'paper') {
+                result = 'loss'
+            } else if (computerSelection == 'scissors'){
+                result = 'win'
+            } else {
+                result = 'draw'
+            }
+            break;
+        case 'scissors':
+            if (computerSelection == 'rock') {
+                result = 'loss'
+            } else if (computerSelection == 'paper'){
+                result = 'win'
+            } else {
+                result = 'draw'
+            }
+            break;
+    }
+    return result
+}
+
+console.log(gameStart(playerChoice, computerChoice))
